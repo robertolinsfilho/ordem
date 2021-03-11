980e09b650f0f58a0cd37326807bb5f4cef8c04c
unknown <rlins74@gmail.com>
2021-03-10 22:58:08 -0300

    att

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,6 +11,10 @@ class Home extends CI_Controller{
     }
     
 public  function index(){
+    $this->load->view('layout/header');
+    
     $this->load->view('home/index');
+    $this->load->view('layout/footer');
+
 }
 }
